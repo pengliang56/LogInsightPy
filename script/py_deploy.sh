bd9878a693066c7bf53192d18f6566024d501acc
liangpeng <247938189@qq.com>
2024-11-03 22:32:10 +0800

    init py and github workflow

--- script/py_deploy.sh
+++ script/py_deploy.sh
@@ -4,7 +4,7 @@
 ps -ef | grep "python"
 
 # shellcheck disable=SC2164
-cd /root/LogInsightPy
+cd ~/LogInsightPy
 
 git pull orign main
 
@@ -14,5 +14,5 @@ cd ~
 # shellcheck disable=SC2046
 [[ -f run.pid ]] && kill $(cat run.pid) && rm ~/run.pid
 
-nohup /usr/bin/python3 ~/LogInsightPy/script/manage.py runserver 127.0.0.1:8000 \
+nohup /usr/bin/python3 ~/LogInsightPy/manage.py runserver 127.0.0.1:8000 \
   > ~/log/app_py_01.log 2>&1 & echo $! > ~/run.pid
