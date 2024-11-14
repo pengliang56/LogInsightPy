950f088ac627696373d6788fe330380316f44a4e
liangpeng <247938189@qq.com>
2024-11-14 20:34:41 +0800

    add tongyi ai

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from apps.view.search_view import search_json
 from apps.view.main_view import main_json
+from apps.view.ai_search import search_ai
 
 urlpatterns = [
     path('api/log/search/', search_json, name='search_json'),
     path('api/log/main/', main_json, name='main_json'),
+    path('api/ai/', search_ai, name='search_ai'),
 ]
