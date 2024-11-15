dfa158ea354045b9e7f5a9d01b04645be86c846c
liangpeng <247938189@qq.com>
2024-11-15 20:23:53 +0800

    fixbug deploy script

--- ai_log/settings.py
+++ ai_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$2e&+hf^^n*4rell!723+6+jzxvtbq++vmg25et1f9h)9qrxp_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 
